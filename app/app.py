313bb74475a367962d62a9752d043f7217e5f99e
Logesh <logeshsmart235@gmail.com>
2026-01-30 00:52:04 +0530

    Final Commit

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.get('/')
 def init():
-    return "App is Running...."
+    return "Vanakam da Maapla, Pipeline correct aa work aaguthu...."
 
 if __name__ == "__main__":
     app.run('0.0.0.0',5000)
