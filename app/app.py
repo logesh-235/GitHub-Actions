ad4addcd1c83d7f975d01d7b6519e8ce850cb96b
Logesh <logeshsmart235@gmail.com>
2026-02-04 07:43:01 +0530

    app.py updated

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.get('/')
 def init():
-    return "Hi Guys..."
+    return "Ashok intha Annamalaya ithuvaraikkum nanbana than pathu iruka"
 
 if __name__ == "__main__":
     app.run('0.0.0.0',5000)
