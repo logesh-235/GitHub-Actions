25d9b6554596dedd9b25ce61c42f61116f7201a0
Logesh <logeshsmart235@gmail.com>
2026-02-04 07:36:14 +0530

    Message changed in app.py code

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.get('/')
 def init():
-    return "Vanakam da Maapla, Pipeline correct aa work aaguthu...."
+    return "Hi Guys..."
 
 if __name__ == "__main__":
     app.run('0.0.0.0',5000)
